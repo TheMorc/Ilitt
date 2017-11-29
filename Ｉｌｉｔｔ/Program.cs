603a84f193df23ba5098edfb41ecd40b13529702
TheMorc <r.gracik@gmail.com>
2017-11-29 16:50:11 +0100

    Add project files.

---
+++ Ｉｌｉｔｔ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ｉｌｉｔｔ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
